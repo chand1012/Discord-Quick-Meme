d2f1da973eb7690ffee19f0d7ac25d1db37e1005
Chandler Lofland <chandlerl14@outlook.com>
2020-01-31 10:44:47 -0500

    more updates to the image search function

--- images.go
+++ images.go
@@ -8,6 +8,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 type imagePayload struct {
@@ -22,11 +24,48 @@ type returnPayload struct {
 	Titles        []string `json:"titles"`
 }
 
-func imageSearch(url string) returnPayload {
+// this if for "how long ago"
+// ex: "5 hours ago"
+// ex: "4 days ago"
+func timeStrToSeconds(stamp string) int64 {
+	var stampLength string
+	var finalTime int64
+	lengths := []string{"second", "minute", "hour", "day", "year"}
+	for _, l := range lengths {
+		if strings.Contains(stamp, l) {
+			stampLength = l
+			break
+		}
+	}
+	spaceIndex := strings.Index(stamp, " ")
+	timeUnknown, _ := strconv.ParseInt(stamp[:spaceIndex], 10, 64)
+	switch stampLength {
+	case "second":
+		finalTime = timeUnknown
+
+	case "minute":
+		finalTime = timeUnknown * 60
+
+	case "hour":
+		finalTime = timeUnknown * 3600
+
+	case "day":
+		finalTime = timeUnknown * 3600 * 24
+
+	case "year":
+		finalTime = timeUnknown * 3600 * 8760
+	}
+	return finalTime
+}
+
+func imageSearch(url string) (string, string) {
 	var payload imagePayload
 	var parsedBody returnPayload
 	payload.ImageURL = url
 	payload.ResizedImages = false
+	var returnURL string
+	var returnTitle string
+	var oldest int64
 
 	data, err := json.Marshal(payload)
 
@@ -35,7 +74,7 @@ func imageSearch(url string) returnPayload {
 	}
 	// this will be changed to a different server that will be hidden in
 	// data.json
-	resp, err := http.Post("http://localhost:5000/search", "application/json", bytes.NewBuffer(data))
+	resp, err := http.Post("http://127.0.0.1:5000/search", "application/json", bytes.NewBuffer(data))
 
 	if err != nil {
 		fmt.Println(err)
@@ -50,7 +89,26 @@ func imageSearch(url string) returnPayload {
 	if err != nil {
 		fmt.Println(err)
 	}
+	urls := parsedBody.Links
+	titles := parsedBody.Descriptions
+	oldest = 0
+	for i := 0; i < len(urls); i++ {
+		if strings.Contains(urls[i], "www.reddit.com/r/") {
+			title := titles[i]
+			dash := strings.Index(title, "-")
+			newTitle := title[dash+2:]
+			dash = strings.Index(newTitle, "-")
+			timeStamp := newTitle[:dash-2]
+			testTime := timeStrToSeconds(timeStamp)
+			fmt.Println(testTime)
+			if testTime >= oldest {
+				oldest = testTime
+				returnURL = urls[i]
+				returnTitle = newTitle
+			}
+		}
+	}
 
-	return parsedBody
+	return returnURL, returnTitle
 
 }
