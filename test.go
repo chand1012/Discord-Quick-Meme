b99f396bd9d848cf7a6b3b54fa1f89cf1a9a1a47
chand1012 <chandlerl14@outlook.com>
2020-04-20 19:21:32 +0000

    started adding tests

---
+++ test.go
@@ -0,0 +1,14 @@
+package main
+
+import "testing"
+
+// Testing
+// Nothing here yet
+
+// Benchmarks
+// BenchmarkRedditConnection testing reddit speed
+func BenchmarkRedditConnection(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_ = PingReddit()
+	}
+}
