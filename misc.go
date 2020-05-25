10330942afc956512c176e38152a89ffb9035528
chand1012 <chandlerl2000@gmail.com>
2020-05-25 12:11:50 -0400

    removed unused function

--- misc.go
+++ misc.go
@@ -11,24 +11,3 @@ func GetMillis() int64 {
 	nanos := now.UnixNano()
 	return nanos / 1000000
 }
-
-// Error checker that I barely used
-// func errCheck(msg string, err error, shouldPanic bool) {
-// 	var errStr string
-// 	if err != nil {
-// 		errStr = err.Error()
-// 		if strings.Contains(errStr, "redis") {
-// 			if !strings.Contains(errStr, "nil") {
-// 				fmt.Printf("%s: %+v\n", msg, err)
-// 				if shouldPanic {
-// 					panic(err)
-// 				}
-// 			}
-// 		} else {
-// 			fmt.Printf("%s: %+v", msg, err)
-// 			if shouldPanic {
-// 				panic(err)
-// 			}
-// 		}
-// 	}
-// }
